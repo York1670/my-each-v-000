9f56dd447d3b3db1b6a05032eefadf254f526e1e
Bobbi York <york1670@gmail.com>
2018-10-01 02:49:42 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,4 +1,8 @@
-def my_each # put argument(s) here
-  # code here
-end
+array = []
 
+def my_each(arrays) # put argument(s) here
+  my_each(array) do |array|
+    puts array
+  end
+  return arrays# code here
+end
